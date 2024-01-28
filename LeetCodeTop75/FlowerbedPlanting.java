f6ad410b2c15006cf05adf7a37095557e5012df6
Kabir Singh <prabhjeezizoo11@gmail.com>
2024-01-27 22:10:41 -0800

    Still need to do flowerbed Problem

--- LeetCodeTop75/FlowerbedPlanting.java
+++ LeetCodeTop75/FlowerbedPlanting.java
@@ -7,7 +7,8 @@ public class FlowerbedPlanting {
         boolean canPlant1 = canPlantFlowers(flowerbed1, n1);
         System.out.println("Example 1: " + canPlant1); // Expected output: true
 
-        int[] flowerbed2 = {1, 0, 0, 0, 1};
+        int[] flowerbed2 = {1,0,0,0,1,0,0};
+
         int n2 = 2;
         boolean canPlant2 = canPlantFlowers(flowerbed2, n2);
         System.out.println("Example 2: " + canPlant2); // Expected output: false
@@ -15,10 +16,10 @@ public class FlowerbedPlanting {
 
     private static boolean canPlantFlowers(int[] flowerbed1, int n1) {
         int countFl= 0;
-        int i=0;
-        while(i<flowerbed1.length){
+        int i=1;
+        while(i<flowerbed1.length-1){
             if(flowerbed1[i]==0){
-                if(i==0 || flowerbed1[i-1]==0){
+                if(i==0 || flowerbed1[i-1]==0 || flowerbed1[i+1]==0){
                     flowerbed1[i]=1;
                     countFl++;
                 }
